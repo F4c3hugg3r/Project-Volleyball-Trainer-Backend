bacd1b9ed5d04f7248ab762cd9403522fb51febd
F4c3hugg3r <l.dubhorn@gmail.com>
2024-05-28 01:21:26 +0200

    Render URL in WebConfig hinzugefügt

--- src/main/java/htwberlin/webtech/config/WebConfig.java
+++ src/main/java/htwberlin/webtech/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5173");
+                .allowedOrigins("http://localhost:5174",
+                        "https://volleyball-trainer-frontend.onrender.com");
     }
 }
