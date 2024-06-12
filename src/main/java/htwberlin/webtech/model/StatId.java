cdea66528b3254706f01dfec7185e4cdedd2974d
F4c3hugg3r <l.dubhorn@gmail.com>
2024-06-12 21:43:48 +0200

    Equals() und HashCode() Methoden für StatId hinzugefügt

--- src/main/java/htwberlin/webtech/model/StatId.java
+++ src/main/java/htwberlin/webtech/model/StatId.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -20,4 +21,17 @@ public class StatId implements Serializable {
     private String id;
     private String rating;
 
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || o.getClass() != this.getClass()) return false;
+        StatId that = (StatId) o;
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.rating, that.rating);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, rating);
+    }
 }
