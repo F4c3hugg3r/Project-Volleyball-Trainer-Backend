8ddd92e46ab9142c3e0e9d54196bf1528d67e3ba
F4c3hugg3r <l.dubhorn@gmail.com>
2024-06-30 21:09:35 +0200

    AA2 hinzugefügt

--- src/main/java/htwberlin/webtech/service/StatsService.java
+++ src/main/java/htwberlin/webtech/service/StatsService.java
@@ -3,22 +3,20 @@ package htwberlin.webtech.service;
 import htwberlin.webtech.model.Stat;
 import htwberlin.webtech.model.StatId;
 import htwberlin.webtech.persistence.StatRepository;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
 
 @Service
 public class StatsService {
 
     public enum Position {
         none,
-        Aussenangreifer,
+        Aussenangreifer1,
         Mittelblocker,
         Libero,
         Zuspieler,
-        Diagonalangreifer
+        Diagonalangreifer,
+        Aussenangreifer2
     }
 
     @Autowired
@@ -44,16 +42,17 @@ public class StatsService {
 
     public boolean deleteStatsByPosition(Integer position) {
         Position positionEnum = switch(position) {
-          case 1 -> Position.Aussenangreifer;
+          case 1 -> Position.Aussenangreifer1;
           case 2 -> Position.Mittelblocker;
           case 3 -> Position.Zuspieler;
           case 4 -> Position.Diagonalangreifer;
           case 5 -> Position.Libero;
+          case 6 -> Position.Aussenangreifer2;
             default -> Position.none;
         };
 
         switch(positionEnum) {
-            case Position.Aussenangreifer -> {
+            case Position.Aussenangreifer1 -> {
                 for(int i=1; i<=12; i++) {
                     for(int j=1; j<=4; j++) {
                         repo.deleteById(new StatId(i, j));
@@ -87,6 +86,14 @@ public class StatsService {
             }
             case Position.Libero -> {
                 for(int i=49; i<=60; i++) {
+                    for(int j=1; j<=4; j++) {
+                        repo.deleteById(new StatId(i, j));
+                    }
+                }
+                return true;
+            }
+            case Position.Aussenangreifer2 -> {
+                for(int i=61; i<=72; i++) {
                     for(int j=1; j<=4; j++) {
                         repo.deleteById(new StatId(i, j));
                     }
